847455d66f96aa790244f26ad30cc4ad49c91de4
Amruth Ayaan Gulawani <144106562+NotCleo@users.noreply.github.com>
2025-05-14 10:41:25 +0530

    Update Medium_Q12.c

--- Medium_Q12.c
+++ Medium_Q12.c
@@ -22,3 +22,34 @@ int main() {
 
     return 0;
 }
+
+
+//Subset counter program
+
+#include<stdio.h>
+void subset_count(int n)
+    {
+        int total = 1 << n;
+        int count = 0;
+        for (int mask = 0; mask < total; mask ++)
+            {
+                printf("{");
+                for(int i = 0; i < n; i++)
+                    {
+                        if(mask & (1<<i))  
+                            {
+                                printf("%d", i+1);
+                            }
+                    }
+                    
+                    printf("}");
+                    printf("\n");
+            }
+    }
+int main()
+    {
+        int n;
+        scanf("%d", &n);
+        subset_count(n);
+        return 0;
+    }
